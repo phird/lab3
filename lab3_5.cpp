2de2cf45cddbf14d4b0db5b27c786a97e5465fe4
phird <59276805+phird@users.noreply.github.com>
2019-12-27 16:22:32 +0700

    lab3_5

---
+++ lab3_5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main (){
+
+string text1, text2 ;
+int N, count=0;
+
+cout << "Enter the First text : ";
+cin >> text1;
+cout << "Enter the Second text : ";
+cin >> text2;
+cout << "Enter N :  ";
+cin >> N;
+while (count<N)
+{
+    if(count%2==0){
+      cout << text1  <<" " ;
+  }
+    else{
+      cout << text2 <<" ";
+        }
+  count=count+1;  
+}
+    return 0;
+}
